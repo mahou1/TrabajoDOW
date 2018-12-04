8a149239e7155d4e9d448f3914264367610aad34
lechuga12 <pablotorres.caceres12@gmail.com>
2018-12-04 14:39:46 -0300

    sin material design

--- Ariel_Pablo/resources/views/guias/index.blade.php
+++ Ariel_Pablo/resources/views/guias/index.blade.php
@@ -9,7 +9,7 @@
   <div class="row ">
     <div class="col">
       <div class="form-group">
-        <a href="/guias/create" class="btn btn-primary">Agregar</a>
+        <a href="/guias/create" class="btn btn-raised btn-primary">Agregar</a>
       </div>
     </div>
   </div>
@@ -31,14 +31,21 @@
               <td>{{$guia->telefono}}</td>
               <td>
 
-                <button type="button" class="btn btn-secondary" data-toggle="snackbar"
-                data-content="<p>{{$guia->nombre}}" 
+                <div class="col">
+                  <h2>{{$guia->nombre}}</h2>
+                  <p>Telefono : {{$guia->telefono}}</p>
+                  <p>Descripcion : {{$guia->descripcion}}</p>
+                </div>
+                <button type="button" class="btn btn-raised btn-secondary" data-toggle="snackbar"
+                data-content="
+                
+                "
                 data-html-allowed="true" data-timeout="0">
-                    Snackbar
+                    Detalles
                 </button>
                 {{Form::open(array('url'=>'guias/'.$guia->id,'method'=>'delete'))}}
-                <button type="submit" name="button" class="btn btn-danger btn-sm">Borrar</button>
-                <a href="/guias/{{$guia->id}}/edit" class="btn btn-success btn-sm">Editar</a>
+                <button type="submit" name="button" class="btn btn-raised btn-danger btn-sm">Borrar</button>
+                <a href="/guias/{{$guia->id}}/edit" class="btn btn-raised btn-success btn-sm">Editar</a>
                 {{Form::close()}}
               </td>
           </tr>
