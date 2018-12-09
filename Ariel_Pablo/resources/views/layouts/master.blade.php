214207371ce653415a69850c4a93b9e4b824c824
mahou1 <34837346+mahou1@users.noreply.github.com>
2018-12-09 12:32:52 -0300

    a

--- Ariel_Pablo/resources/views/layouts/master.blade.php
+++ Ariel_Pablo/resources/views/layouts/master.blade.php
@@ -14,7 +14,7 @@
   <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.5.15/css/mdb.min.css" rel="stylesheet">
   <!-- Your custom styles (optional) -->
   <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
-
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
   <title>Document</title>
   <style >
     .titulo-form{
@@ -23,63 +23,83 @@
   </style>
 </head>
 <body>
-
   <nav class="navbar navbar-expand-lg  navbar-dark bg-dark scrolling-navbar fixed-top ">
-    <div class="container">
-      <a class="navbar-brand" href="/">Inicio</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item {{Request::segment(1)=='tours'? 'active':''}}">
-            <a class="nav-link" href="/tours">Tours</a>
-          </li>
+    <div class="container ">
+        <div>
+          <a class="navbar-brand" href="/">Inicio</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        </div>
+        <div>
+          <div class="dropdown dropleft d-block d-md-none">
+            <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                <i style="font-size: 2rem; color: white;" class="fas fa-user"></i>
+            </a>
+          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            @guest
+                  <a class="dropdown-item" href="/login">Iniciar Sesión</a>
+                  <a class="dropdown-item"href="/usuarios/create">Registrarse</a>
+            @endguest
+            @auth
 
-          @auth
-            @if(auth()->user()->tipo==='Administrador')
-              {{-- @can('create',App\Tour::class) --}}
-              <li class="nav-item {{Request::segment(1)=='sesiones'? 'active':''}}">
-                <a class="nav-link"href="/sesiones">Sesiones</a>
-              </li>
-              <li class="nav-item {{Request::segment(1)=='guias'? 'active':''}}">
-                <a class="nav-link"href="/guias">Guias</a>
-              </li>
-              <li class="nav-item {{Request::segment(1)=='ubicaciones'? 'active':''}}">
-                <a class="nav-link"href="/ubicaciones">Ubicaciones</a>
-              </li>
-            @endif
-          @endauth
-        </ul>
-      </div>
+                  <a class="dropdown-item"href="#">{{ Auth::user()->nombre_completo }}</a>
 
-      @guest
-        <ul class="navbar-nav flex-row ml-md-auto d-none d-md-flex ">
-          <li class="nav-item">
-            <a class="nav-link" href="/login">Iniciar Sesión</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link"href="/usuarios/create">Registrarse</a>
-          </li>
+                  <a class="dropdown-item"href="/logout">Salir</a>
 
-        </ul>
-      @endguest
-      @auth
-        <ul class="navbar-nav flex-row ml-md-auto d-none d-md-flex ">
-          <li class="nav-item">
-            <a class="nav-link"href="#">{{ Auth::user()->nombre_completo }}</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link"href="/logout">Salir</a>
-          </li>
-        </ul>
-      @endauth
+            @endauth
+
+          </div>
+          </div>
+
+        </div>
+        <div class=" collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ">
+            <li class="nav-item {{Request::segment(1)=='tours'? 'active':''}}">
+              <a class="nav-link" href="/tours">Tours</a>
+            </li>
+
+            @auth
+              @if(auth()->user()->tipo==='Administrador')
+                {{-- @can('create',App\Tour::class) --}}
+                <li class="nav-item {{Request::segment(1)=='sesiones'? 'active':''}}">
+                  <a class="nav-link"href="/sesiones">Sesiones</a>
+                </li>
+                <li class="nav-item {{Request::segment(1)=='guias'? 'active':''}}">
+                  <a class="nav-link"href="/guias">Guias</a>
+                </li>
+                <li class="nav-item {{Request::segment(1)=='ubicaciones'? 'active':''}}">
+                  <a class="nav-link"href="/ubicaciones">Ubicaciones</a>
+                </li>
+              @endif
+            @endauth
+          </ul>
+        </div>
+        @guest
+          <ul class="navbar-right navbar-nav flex-row ml-md-auto d-none d-md-flex ">
+            <li class="nav-item">
+              <a class="nav-link" href="/login">Iniciar Sesión</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link"href="/usuarios/create">Registrarse</a>
+            </li>
+          </ul>
+        @endguest
+        @auth
+          <ul class="navbar-nav flex-row ml-md-auto d-none d-md-flex ">
+            <li class="nav-item">
+              <a class="nav-link"href="#">{{ Auth::user()->nombre_completo }}</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link"href="/logout">Salir</a>
+            </li>
+          </ul>
+        @endauth
     </div>
   </nav>
 
-
   <div class="d-flex justify-content-center align-items-center">
-    <div class="container pt-5 mt-5 mb-5">
+    <div class="container pt-5 mt-5 mb-5  mh-auto">
       @yield('contenido')
     </div>
   </div>
@@ -87,14 +107,14 @@
   <!-- Footer -->
   <footer class="page-footer font-small gris pt-4">
       <div class="container">
-        <ul class="list-unstyled list-inline text-center py-2">
+        {{-- <ul class="list-unstyled list-inline text-center py-2">
           <li class="list-inline-item">
             <a href="/login" class="btn btn-outline-primary btn-rounded">Iniciar Sesión</a>
           </li>
           <li class="list-inline-item">
             <a href="/usuarios/create" class="btn btn-outline-white btn-rounded">Registrarse</a>
           </li>
-        </ul>
+        </ul> --}}
       </div>
       <div class="footer-copyright text-center py-3">
 
