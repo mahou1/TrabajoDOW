3e14a5345c5f1df81c9f0314028ca33a0d21ef51
lechuga12 <pablotorres.caceres12@gmail.com>
2018-12-09 02:31:52 -0300

    a

--- Ariel_Pablo/resources/views/login/login.blade.php
+++ Ariel_Pablo/resources/views/login/login.blade.php
@@ -11,13 +11,17 @@
     <div class="card">
       <div class="card-body">
         {{Form::open(array('url'=>'login'))}}
-        <div class="form-group">
-          <label for="user">Nombre de usuario</label>
-          <input type="tex" name="user" class="form-control">
+        <div class="md-form">
+          {{-- <label for="user">Nombre de usuario</label> --}}
+          <i class="fas fa-user prefix"></i>
+          <input type="text" name="user" id="user" class="form-control">
+          <label for="user" id="user">Nombre de usuario</label>
         </div>
-        <div class="form-group">
-          <label for="password">Contraseña</label>
-          <input type="password" name="password" class="form-control">
+        <div class="md-form">
+          {{-- <label for="password">Contraseña</label> --}}
+          <i class="fas fa-key prefix"></i>
+          <input type="password" name="password" id="password"class="form-control">
+          <label for="password" id="password">Contraseña</label>
         </div>
         <button type="submit" name="iniciar" class="btn btn-primary">Iniciar sesión</button>
         {{Form::close()}}
--- Ariel_Pablo/resources/views/usuarios/create.blade.php
+++ Ariel_Pablo/resources/views/usuarios/create.blade.php
@@ -1,19 +1,17 @@
 @extends('layouts.master')
 @section('contenido')
   <div class="row">
-    <div class="col">
-      <h2>Registrar cuenta</h2>
+    <div class="col-md-6 mb-5 dark-text text-center text-md-left">
+      <h2 class="display-4 mb-0 pt-md-5 pt-5 font-weight-bold ">Registrar cuenta</h2>
     </div>
-  </div>
-  <hr/>
-  <div class="row">
+
     <div class="col-md-6">
-          {{Form::open(array('url'=>'/usuarios','method'=>'post'))}}
-      <div class="card ">
+      {{Form::open(array('url'=>'/usuarios','method'=>'post'))}}
+      <div class="card">
         <div class="card-body">
-            <div class="form-group">
-              <label class=for="txtUser">User :</label>
-              <input type="text" class="form-control {{($errors->has('user')?'is-invalid':'')}}" name="user" values="{{ old('user') }}">
+            <div class="md-form">
+              <i class="fas fa-user prefix"></i>
+              <input type="text" id="txtUser"class="form-control {{($errors->has('user')?'is-invalid':'')}}" name="user" values="{{ old('user') }}">
               @if($errors->has('user'))
                 <div class=" alert alert-danger p-0 mt-2 mb-0" rol="alert">
                   <ul>
@@ -23,10 +21,11 @@
                   </ul>
                 </div>
               @endif
+              <label for="txtUser" id="txtUser">Nombre Usuario</label>
             </div>
-            <div class="form-group">
-              <label for="txtPassword">Contraseña :</label>
-              <input type="text" class="form-control {{$errors->has('password')?'is-invalid':''}}" name="password">
+            <div class="md-form">
+              <i class="fas fa-key prefix"></i>
+              <input type="text" id="txtPassword" class="form-control {{$errors->has('password')?'is-invalid':''}}" name="password">
               @if($errors->has('password'))
                 <div class=" alert alert-danger p-0 mt-2 mb-0" rol="alert">
                   <ul>
@@ -36,10 +35,11 @@
                   </ul>
                 </div>
               @endif
+              <label for="txtPassword" id="txtPassword">Contraseña</label>
             </div>
-            <div class="form-group">
-              <label for="txtpassword_comfirmation">Repita contraseña :</label>
-              <input type="text" class="form-control  {{$errors->has('password_confirmation')?'is-invalid':''}}" name="password_confirmation">
+            <div class="md-form">
+              <i class="fas fa-key prefix"></i>
+              <input type="text" id="txtpassword_comfirmation" class="form-control  {{$errors->has('password_confirmation')?'is-invalid':''}}" name="password_confirmation">
               @if($errors->has('password_confirmation'))
                 <div class=" alert alert-danger p-0 mt-2 mb-0" rol="alert">
                   <ul>
@@ -49,10 +49,12 @@
                   </ul>
                 </div>
               @endif
+              <label for="txtpassword_comfirmation" id="txtpassword_comfirmation">Repita contraseña</label>
             </div>
-            <div class="form-group">
-              <label for="txtnombre-completo">Nombre :</label>
-              <input type="text" class="form-control  {{$errors->has('nombre_completo')?'is-invalid':''}}" name="nombre_completo">
+            <hr/>
+            <div class="md-form">
+              <i class="fas fa-user-tie prefix"></i>
+              <input type="text"  id="txtnombre-completo" class="form-control  {{$errors->has('nombre_completo')?'is-invalid':''}}" name="nombre_completo">
               @if($errors->has('nombre_completo'))
                 <div class=" alert alert-danger p-0 mt-2 mb-0" rol="alert">
                   <ul>
@@ -62,10 +64,11 @@
                   </ul>
                 </div>
               @endif
+              <label for="txtnombre-completo" id="txtnombre-completo">Nombre</label>
             </div>
-            <div class="form-group">
-              <label for="txtCorreo">Correo :</label>
-              <input type="email" class="form-control   {{$errors->has('correo')?'is-invalid':''}}" name="correo">
+            <div class="md-form">
+              <i class="fas fa-envelope prefix"></i>
+              <input type="email"  id="txtCorreo"class="form-control   {{$errors->has('correo')?'is-invalid':''}}" name="correo">
               @if($errors->has('correo'))
                 <div class=" alert alert-danger p-0 mt-2 mb-0" rol="alert">
                   <ul>
@@ -75,12 +78,14 @@
                   </ul>
                 </div>
               @endif
+              <label for="txtCorreo" id="txtCorreo">Correo</label>
             </div>
-            <div class="form-group row d-flex">
-              <label class="col col-md-5"for="txtFecha-nac">Fecha de Nacimiento :</label>
-              <div class="col col-md-6">
-                  <input type="date" class=" {{$errors->has('fecha_nac')?'is-invalid':''}} form-control" name="fecha_nac">
-              </div>
+            <div class="md-form">
+
+              <i class="fas fa-birthday-cake prefix"></i>
+
+              <input type="date" id="txtFecha-nac" class=" {{$errors->has('fecha_nac')?'is-invalid':''}} form-control" name="fecha_nac">
+
               @if($errors->has('fecha_nac'))
                 <div class="col">
                   <div class="alert alert-danger p-0 mt-2 mb-0" role="alert">
@@ -93,17 +98,17 @@
 
                 </div>
               @endif
+              {{-- <label class="col col-md-5" id="txtFecha-nac"for="txtFecha-nac">Fecha de Nacimiento</label> --}}
             </div>
-            <div class="form-group row ">
-              <label class="col col-md-4"for="txtGenero">Genero : </label>
-              <div class="col col-md-4">
-                <select class="custom-select form-control {{$errors->has('genero')?'is-invalid':''}}" name="genero">
-                  <option value="" selected >Seleccione</option>
-                  <option value="M">Masculino</option>
-                  <option value="F">Femenino</option>
-                  <option value="O">Otro</option>
-                </select>
-                </div>
+            <div class="form-group">
+              <i class="fas fa-genderless prefix"></i>
+              <label class="col col-md-5" id="txtGenero"for="txtFecha-nac">Genero</label>
+              <select  id="txtGenero" type="selected"class="custom-select form-control md-form{{$errors->has('genero')?'is-invalid':''}}" name="genero">
+                <option value="" selected>Seleccione</option>
+                <option value="M">Masculino</option>
+                <option value="F">Femenino</option>
+                <option value="O">Otro</option>
+              </select>
                 @if ($errors->has('genero'))
                   <div class="col-12">
                     <div class="alert alert-danger p-0 mt-2 mb-0" role="alert">
@@ -125,6 +130,10 @@
       </div>
       {{Form::close()}}
     </div>
-
   </div>
+
+  <hr/>
+
+
+
 @endsection
